f286f82eff454d10d034cd460198c78532330446
Joshua Chan <JChan@ucvts.tec.nj.us>
2019-10-29 13:07:27 -0400

    First commit

--- src/change.js
+++ src/change.js
@@ -3,12 +3,27 @@ const readlineSync = require("readline-sync");
 const MIN = 0.01;
 const MAX = 0.99;
 
-const amount = Number(readlineSync.question("\nEnter a dollar amount: "));
+let amount = Number(readlineSync.question("\nEnter a dollar amount: "));
+
+const QUARTER_VALUE = 0.25;
+const DIME_VALUE = 0.1;
+const NICKEL_VALUE = 0.05
 
 if (Number.isNaN(amount)) {
-    // print your error message here
+  console.log("Invalid")
 } else if (amount < MIN || amount > MAX) {
-    // print your error message here
+  console.log("Invalid")
 } else {
-    // write your code for making change here
+    let q = Math.floor(amount / QUARTER_VALUE);
+    amount = amount % QUARTER_VALUE;
+
+    let d = Math.floor(amount / DIME_VALUE);
+    amount = amount % DIME_VALUE;
+
+    let n = Math.floor(amount / NICKEL_VALUE);
+    amount = amount % NICKEL_VALUE;
+
+    let p = Math.round(amount * 100);
+
+    console.log("\n" + q + " quarters, " + d + " dimes, " + n + " nickels, " + p + " pennies.");
 }
