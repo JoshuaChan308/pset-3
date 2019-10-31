3b7bec8b112c32eb02d0cc59104ed3ad6b74024f
Joshua Chan <JChan@ucvts.tec.nj.us>
2019-10-31 12:32:34 -0400

    parity

--- src/sign.js
+++ src/sign.js
@@ -2,7 +2,7 @@ const readlineSync = require("readline-sync");
 
 let number = Number(readlineSync.question("\nEnter a number: "));
 
-  if (Number.isNaN(number)) {
+if (Number.isNaN(number)) {
     console.log("\nInvalid.");
 } else if (number < Number.MIN_SAFE_INTEGER || number > Number.MAX_SAFE_INTEGER) {
     console.log("\nInvalid.");
