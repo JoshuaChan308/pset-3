6372b5c11db697c2b1c3077f0e1a92cfa69eed5e
Joshua Chan <JChan@ucvts.tec.nj.us>
2019-11-06 13:18:29 -0500

    years

--- src/years.js
+++ src/years.js
@@ -6,25 +6,25 @@ if (!Number.isNaN(year)) {
   if (year>0 && year<= Number.MAX_SAFE_INTEGER) {
     if (year%4==0) {
       if (year%100!=0) {
-        console.log("\n" + year + " is a leap year.\n");
+        console.log("\n" + year + " is a leap year.");
       }
       else {
         if (year%400==0) {
-          console.log("\n" + year + " is a leap year.\n");
+          console.log("\n" + year + " is a leap year.");
         }
         else {
-          console.log("\n" + year + " is not a leap year.\n");
+          console.log("\n" + year + " is not a leap year.");
         }
       }
     }
     else {
-      console.log("\n" + year + " is not a leap year.\n");
+      console.log("\n" + year + " is not a leap year.");
     }
   }
   else {
-    console.log("\nInvalid.\n");
+    console.log("\nInvalid.");
   }
 }
 else {
-    console.log("\nInvalid.\n");
+    console.log("\nInvalid.");
 }
